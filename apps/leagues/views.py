6fe92f8842f637cfe5e155c8c3f9ad75b61aaf92
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-07-17 19:30:58 -0700

    added Level 2 queries

--- apps/leagues/views.py
+++ apps/leagues/views.py
@@ -4,7 +4,7 @@ from .models import League, Team, Player
 from . import team_maker
 
 def index(request):
-	context = {
+	''' context = {
 		"leagues": League.objects.filter(sport="Baseball")
 	}
 
@@ -67,6 +67,69 @@ def index(request):
 	context = {
 		"players": Player.objects.filter(first_name="Alexander")|Player.objects.filter(first_name="Wyatt")
 	}
+
+	context = {
+		"teams": League.objects.get(name="Atlantic Soccer Conference").teams.all()
+	}
+
+	context = {
+		"players": Team.objects.get(team_name="Penguins", location="Boston").curr_players.all()
+	}
+
+	context = {"players": []}
+	teams = League.objects.get(name="International Collegiate Baseball Conference").teams.all()
+	for team in teams:
+		team_players = team.curr_players.all()
+		for player in team_players:
+			context['players'].append(player)
+	
+	context = {"players": []}
+	teams = League.objects.get(name="American Conference of Amateur Football").teams.all()
+	for team in teams:
+		team_players = team.curr_players.all().filter(last_name="Lopez")
+		for player in team_players:
+			context['players'].append(player)
+	
+	context = {"players": []}
+	leagues = League.objects.filter(sport="Football")
+	for league in leagues:
+		teams = league.teams.all()
+		for team in teams:
+			team_players = team.curr_players.all()
+			for player in team_players:
+				context['players'].append(player)
+	
+	context = {"teams": []}
+	teams = Team.objects.all()
+	for team in teams:
+		players = team.curr_players.all()
+		for player in players:
+			if player.first_name == "Sophia":
+				context['teams'].append(team)
+				break
+	
+	context = {"leagues": []}
+	player_found = False
+	leagues = League.objects.all()
+	for league in leagues:
+		player_found = False
+		teams = league.teams.all()
+		for team in teams:
+			if player_found == True:
+				break
+			players = team.curr_players.all()
+			for player in players:
+				if player.first_name == "Sophia":
+					context['leagues'].append(league)
+					player_found = True
+					break '''
+	
+	context = {"players": []}
+	players = Player.objects.filter(last_name="Flores")
+	for player in players:
+		if player.curr_team.team_name != "Roughriders" and player.curr_team.location != "Washington":
+			context['players'].append(player)
+
 
 	return render(request, "leagues/index.html", context)
 
